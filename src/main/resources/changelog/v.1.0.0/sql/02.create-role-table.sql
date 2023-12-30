2391f8b2c80d6370de64dfd9f31c95bc1b041735
chernickij <natalia.smoliak309@gmail.com>
2023-12-31 02:30:15 +0300

    add multi modules structure and open api configs for the book service

--- src/main/resources/changelog/v.1.0.0/sql/02.create-role-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS role
-(
-    id         serial             NOT NULL,
-    name       varchar(25) UNIQUE NOT NULL,
-    ts_created timestamp          NOT NULL,
-    ts_updated timestamp,
-    PRIMARY KEY (id)
-);
-
--- src/main/resources/changelog/v.1.0.0/sql/06.create-genre-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS genre
-(
-    id         serial       NOT NULL,
-    name       varchar(255) UNIQUE NOT NULL,
-    ts_created timestamp    NOT NULL,
-    ts_updated timestamp,
-    PRIMARY KEY (id)
-);
